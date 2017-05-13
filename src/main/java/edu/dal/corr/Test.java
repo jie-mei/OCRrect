5a3f448b3aab392f85b965535717df56657c8b52
jMeillness <mei.jie@hotmail.com>
2017-05-13 18:02:04 -0300

    Add candidate generation.

--- src/main/java/edu/dal/corr/Test.java
+++ src/main/java/edu/dal/corr/Test.java
@@ -1,10 +1,12 @@
 package edu.dal.corr;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.BitSet;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -66,10 +68,10 @@ public class Test {
   public static void main(String[] args) throws IOException {
 
     // Read pre-processed 5-gram search indexing.
-    NgramBoundedReaderSearcher bigram = getNgramSearch("2gm.search", ResourceUtils.BIGRAM);
-    NgramBoundedReaderSearcher trigram = getNgramSearch("3gm.search", ResourceUtils.TRIGRAM);
-    NgramBoundedReaderSearcher fourgram = getNgramSearch("4gm.search", ResourceUtils.FOURGRAM);
-    NgramBoundedReaderSearcher fivegram = getNgramSearch("5gm.search", ResourceUtils.FIVEGRAM);
+//    NgramBoundedReaderSearcher bigram = getNgramSearch("2gm.search", ResourceUtils.BIGRAM);
+//    NgramBoundedReaderSearcher trigram = getNgramSearch("3gm.search", ResourceUtils.TRIGRAM);
+//    NgramBoundedReaderSearcher fourgram = getNgramSearch("4gm.search", ResourceUtils.FOURGRAM);
+//    NgramBoundedReaderSearcher fivegram = getNgramSearch("5gm.search", ResourceUtils.FIVEGRAM);
 
     // Read words from input text.
     List<Word> words = Word.tokenize(
@@ -83,63 +85,85 @@ public class Test {
     List<Boolean> trainLabels = labelWords(trainWords);
     List<Boolean> testLabels = labelWords(testWords);
 
-    Detectable[] features = {
-      new WordValidityFeature(IOUtils.readList(ResourceUtils.VOCAB)),
-      new CharacterExistenceFeature((char)32),
-      new CharacterExistenceFeature((char)33),
-      new CharacterExistenceFeature((char)34),
-      new CharacterExistenceFeature((char)35),
-      new CharacterExistenceFeature((char)36),
-      new CharacterExistenceFeature((char)37),
-      new CharacterExistenceFeature((char)38),
-      new CharacterExistenceFeature((char)39),
-      new CharacterExistenceFeature((char)40),
-      new CharacterExistenceFeature((char)41),
-      new CharacterExistenceFeature((char)42),
-      new CharacterExistenceFeature((char)43),
-      new CharacterExistenceFeature((char)44),
-      new CharacterExistenceFeature((char)45),
-      new CharacterExistenceFeature((char)46),
-      new CharacterExistenceFeature((char)47),
-      new CharacterExistenceFeature((char)58),
-      new CharacterExistenceFeature((char)59),
-      new CharacterExistenceFeature((char)60),
-      new CharacterExistenceFeature((char)61),
-      new CharacterExistenceFeature((char)62),
-      new CharacterExistenceFeature((char)63),
-      new CharacterExistenceFeature((char)64),
-      new CharacterExistenceFeature((char)91),
-      new CharacterExistenceFeature((char)92),
-      new CharacterExistenceFeature((char)93),
-      new CharacterExistenceFeature((char)94),
-      new CharacterExistenceFeature((char)95),
-      new CharacterExistenceFeature((char)96),
-      new CharacterExistenceFeature((char)123),
-      new CharacterExistenceFeature((char)124),
-      new CharacterExistenceFeature((char)125),
-      new CharacterExistenceFeature((char)126),
-      new CharacterExistenceFeature((char)127),
-      new ContextCoherenceFeature("bigram", bigram, 2),
-      new ContextCoherenceFeature("trigram", trigram, 3),
-      new ContextCoherenceFeature("fourgram", fourgram, 4),
-      new ContextCoherenceFeature("fivegram", fivegram, 5),
-      new ApproximateContextCoherenceFeature("bigram", bigram, 2),
-      new ApproximateContextCoherenceFeature("trigram", trigram, 3),
-      new ApproximateContextCoherenceFeature("fourgram", fourgram, 4),
-      new ApproximateContextCoherenceFeature("fivegram", fivegram, 5),
-    };
+//    Detectable[] features = {
+//      new WordValidityFeature(IOUtils.readList(ResourceUtils.VOCAB)),
+//      new CharacterExistenceFeature((char)32),
+//      new CharacterExistenceFeature((char)33),
+//      new CharacterExistenceFeature((char)34),
+//      new CharacterExistenceFeature((char)35),
+//      new CharacterExistenceFeature((char)36),
+//      new CharacterExistenceFeature((char)37),
+//      new CharacterExistenceFeature((char)38),
+//      new CharacterExistenceFeature((char)39),
+//      new CharacterExistenceFeature((char)40),
+//      new CharacterExistenceFeature((char)41),
+//      new CharacterExistenceFeature((char)42),
+//      new CharacterExistenceFeature((char)43),
+//      new CharacterExistenceFeature((char)44),
+//      new CharacterExistenceFeature((char)45),
+//      new CharacterExistenceFeature((char)46),
+//      new CharacterExistenceFeature((char)47),
+//      new CharacterExistenceFeature((char)58),
+//      new CharacterExistenceFeature((char)59),
+//      new CharacterExistenceFeature((char)60),
+//      new CharacterExistenceFeature((char)61),
+//      new CharacterExistenceFeature((char)62),
+//      new CharacterExistenceFeature((char)63),
+//      new CharacterExistenceFeature((char)64),
+//      new CharacterExistenceFeature((char)91),
+//      new CharacterExistenceFeature((char)92),
+//      new CharacterExistenceFeature((char)93),
+//      new CharacterExistenceFeature((char)94),
+//      new CharacterExistenceFeature((char)95),
+//      new CharacterExistenceFeature((char)96),
+//      new CharacterExistenceFeature((char)123),
+//      new CharacterExistenceFeature((char)124),
+//      new CharacterExistenceFeature((char)125),
+//      new CharacterExistenceFeature((char)126),
+//      new CharacterExistenceFeature((char)127),
+//      new ContextCoherenceFeature("bigram", bigram, 2),
+//      new ContextCoherenceFeature("trigram", trigram, 3),
+//      new ContextCoherenceFeature("fourgram", fourgram, 4),
+//      new ContextCoherenceFeature("fivegram", fivegram, 5),
+//      new ApproximateContextCoherenceFeature("bigram", bigram, 2),
+//      new ApproximateContextCoherenceFeature("trigram", trigram, 3),
+//      new ApproximateContextCoherenceFeature("fourgram", fourgram, 4),
+//      new ApproximateContextCoherenceFeature("fivegram", fivegram, 5),
+//    };
+    
+    writeWords(trainWords, Paths.get("train-detect.words.txt"));
+    writeWords(testWords, Paths.get("test-detect.words.txt"));
+    writeLabels(testLabels, Paths.get("test-detect.label.gt.txt"));
 
-    SVMEstimator svm = new SVMEstimator("/home/default/miniconda3/bin/python", features);
-    svm.train(trainWords, trainLabels);
+//    SVMEstimator svm = new SVMEstimator("/home/default/miniconda3/bin/python", features);
+//    svm.train(trainWords, trainLabels);
+//
+//    boolean[] predLabels = svm.predict(testWords);
+//    float correct = 0;
+//    for (int i = 0; i < predLabels.length; i++) {
+//      if (testLabels.get(i) == predLabels[i]) {
+//        correct++;
+//      }
+//    }
+//    System.out.println(String.format("%d / %d = %.4f",
+//        (int)correct, predLabels.length, correct / predLabels.length));
+  }
 
-    boolean[] predLabels = svm.predict(testWords);
-    float correct = 0;
-    for (int i = 0; i < predLabels.length; i++) {
-      if (testLabels.get(i) == predLabels[i]) {
-        correct++;
-      }
+  private static void writeWords(List<Word> words, Path path) throws IOException {
+    try (BufferedWriter bw = Files.newBufferedWriter(path, StandardOpenOption.CREATE_NEW)) {
+      bw.write(words
+          .stream()
+          .map(w -> w.text())
+          .collect(Collectors.joining("\n")));
     }
-    System.out.println(String.format("%d / %d = %.4f",
-        (int)correct, predLabels.length, correct / predLabels.length));
+  }
+  
+  private static void writeLabels(List<Boolean> labels, Path path) throws IOException {
+    try (BufferedWriter bw = Files.newBufferedWriter(path, StandardOpenOption.CREATE_NEW)) {
+      bw.write(labels
+          .stream()
+          .map(l -> l ? "1" : "0")
+          .collect(Collectors.joining("\t")));
+    }
   }
 }
