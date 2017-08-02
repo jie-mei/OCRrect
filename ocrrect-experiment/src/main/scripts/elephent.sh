84d165aac23da13f62e6eed3301c4cd4f74e588b
jmei91 <mei.jie@hotmail.com>
2017-08-02 16:32:29 -0300

    Add elephent script for tokenization.

---
+++ ocrrect-experiment/src/main/scripts/elephent.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# Arguments:
+#   1: A text string to be tokenized.
+
+TEMP_PATH='tmp'
+REPO_URL='https://github.com/jmei91/elephant'
+
+# Check arguments.
+if [ "$#" -ne 1 ]; then
+  echo "Usage: ${0} TEXT"
+  exit 1
+fi
+
+# Download elephant source code if not exist.
+if [ ! -e "${TEMP_PATH}/elephant" ]; then
+  git clone "${REPO_URL}" "${TEMP_PATH}/elephant"
+fi
+
+# Perform tokenization using elephant.
+tokens=`cd "${TEMP_PATH}/elephant" && echo "${1}" | python2 elephant -m models/english`
+
+# Print tokens in separated lines.
+for tk in ${tokens}; do
+  echo ${tk}
+done
